d9c9a4964529be2bb6a3ce875a07ff9dcbb0e0db
Klebbleboopb <tolbertisaiah290@gmail.com>
2026-09-07 10:08:46 -0700

    Update Preprocessed_BundledReal.lua

--- Preprocessed_BundledReal.lua
+++ Preprocessed_BundledReal.lua
@@ -87285,7 +87285,7 @@ function Hooking.init()
 	local clientManager = clientActor:WaitForChild("ClientManager")
 	local requests = replicatedStorage:WaitForChild("Requests")
 
-	task.wait(5)
+	task.wait(10)
 
 	---@note: Crucial part because of the actor and the error detection.
 	clientManager.Enabled = false
@@ -87303,7 +87303,7 @@ function Hooking.init()
 	end
 
 	-- Did we execute with a standalone AC bypass?
-	task.wait(3)
+	task.wait()
 
 	local nulledBanRemotes = {}
 
