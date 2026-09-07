bbd7c334127b2d036a0218fad66bf48410699eef
Klebbleboopb <tolbertisaiah290@gmail.com>
2026-09-07 09:48:02 -0700

    Update Preprocessed_BundledReal.lua

--- Preprocessed_BundledReal.lua
+++ Preprocessed_BundledReal.lua
@@ -87284,8 +87284,8 @@ function Hooking.init()
 	local clientActor = playerScripts:WaitForChild("ClientActor")
 	local clientManager = clientActor:WaitForChild("ClientManager")
 	local requests = replicatedStorage:WaitForChild("Requests")
-  clientManager.Enabled = false
-	task.wait(3)
+  
+	task.wait(1)
 	---@note: Crucial part because of the actor and the error detection.
 	clientManager.Enabled = true
 
