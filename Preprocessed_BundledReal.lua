ac0d0c14922777a7899959bb9836d354c889e75f
Klebbleboopb <tolbertisaiah290@gmail.com>
2026-09-07 09:46:34 -0700

    Update Preprocessed_BundledReal.lua

--- Preprocessed_BundledReal.lua
+++ Preprocessed_BundledReal.lua
@@ -87284,10 +87284,11 @@ function Hooking.init()
 	local clientActor = playerScripts:WaitForChild("ClientActor")
 	local clientManager = clientActor:WaitForChild("ClientManager")
 	local requests = replicatedStorage:WaitForChild("Requests")
-
+  clientManager.Enabled = false
+	task.wait(3)
 	---@note: Crucial part because of the actor and the error detection.
-	clientManager.Enabled = false
-  task.wait(1)
+	clientManager.Enabled = true
+
 	---@note: Dynamically get the ban remotes.
 	local banRemoteCount = 0
 
